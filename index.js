4e2c457f4e4f6ce404825ab97dcbb6d13a37d22e
Troy <troyakins99@yahoo.com>
2021-06-19 14:06:59 -0500

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
 // Write your code here!
+document.querySelector('main').remove();
+
+let newHeader = document.createElement('H1');
+document.body.appendChild(newHeader);
+newHeader.id='victory';
+newHeader.innerHTML = "Troy is the champion"
+
+
+
+
